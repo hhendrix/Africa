88db27ec3ff47e406e4ff90bfd3aaeab46d0192a
Harry Lopez <harry.lopez@overactive.com>
2022-04-07 15:45:37 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Harry Lopez on 7/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
